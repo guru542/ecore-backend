8922b8f37778344bbc9d1619b2fb417ac1af55dc
Guru542 <guruprasadgp335@gmail.com>
2026-02-03 20:06:31 +0530

    Enable CORS for frontend

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.get("/test", (req, res) => {
+  res.send("Backend working");
+});
 
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
